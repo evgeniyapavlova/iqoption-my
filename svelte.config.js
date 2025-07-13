ca05c4513990bf9d66b84b1fb6577d29f6127261
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-13 05:44:27 +0300

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,9 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/iqoption-my' : ''
-		}
+		// paths: {
+		// 	base: process.env.NODE_ENV === 'production' ? '/iqoption-my' : ''
+		// }
 	}
 };
 
