2463db69cf103a0e4b972eaa435ea71d1d2411e6
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-13 04:11:30 +0300

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script>
+	import './styles.css';
+
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { iframe_params } from '../lib/constants/reg_link';
+
+	export const continentCode = writable('');
+
+	let loading = false;
+
+	function updateLinks() {
+		iframe_params.set(window.location.search);
+	}
+
+	onMount(() => {
+		updateLinks();
+	});
+</script>
+
+{#if loading}
+	<div class="wrap">
+		<div class="loader"></div>
+	</div>
+{:else}
+	<div class="app" dir="rtl">
+		<slot />
+	</div>
+{/if}
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+	.wrap {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+	}
+	.loader {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		position: relative;
+		animation: rotate 1s linear infinite;
+	}
+	.loader::before {
+		content: '';
+		box-sizing: border-box;
+		position: absolute;
+		inset: 0px;
+		border-radius: 50%;
+		border: 5px solid var(--orange);
+		animation: prixClipFix 2s linear infinite;
+	}
+
+	@keyframes rotate {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	@keyframes prixClipFix {
+		0% {
+			clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+		}
+		25% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+		}
+		50% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+		}
+		75% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+		}
+		100% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { base } from '$app/paths';
+	import meta__img from '$lib/images/meta__img.jpg';
+	import AllBlocks from '../components/AllBlocks.svelte';
+
+	const title =
+		'لنتعرَّف على منصَّة IQ Option - ما هي وما آلية عملها كوسيط مالي، مع نظرة على الحساب الإسلامي لديها';
+	const desc =
+		'تُعدُّ IQ Option منصَّة تداوُل إلكترونية مُتاحة عبر الإنترنت، مُصمَّمة لتلبية طيف واسع من التفضيلات الاستثمارية لدى العملاء. تضمُّ قاعدة مستخدمين واسعة، مع ملايين الأعضاء المُسجَّلين لديها عبر ما يزيد عن 213 دولة حول العالم. ستجد في هذا الدليل الإرشاديّ الشامل إجاباتٍ لكل ما يخطر ببالك حول منصَّة IQ Option، بدءًا من أبرز مزاياها الأساسية ووصولًا إلى الوظائف المتقدِّمة، بما يشمل ميزة الحساب الإسلامي على وجه الخصوص.';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={desc} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={desc} />
+	<meta property="og:image" content={meta__img} />
+
+	<meta name="twitter:site" content="iq-trade.br.com" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={desc} />
+	<meta name="twitter:image" content={meta__img} />
+	<link
+		rel="preload"
+		as="image"
+		href="{base}/images/bg_intro.webp"
+		type="image/webp"
+		fetchpriority="high"
+	/>
+</svelte:head>
+
+<AllBlocks />
